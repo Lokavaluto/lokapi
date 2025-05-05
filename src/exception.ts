5bf3f747d0e4949081b6b07e8975722a94a4a57d
Valentin Lab <valentin.lab@kalysto.org>
2025-05-05 10:56:27 +0200

    new: add new ``PrepareTransfer*`` exceptions
    These exceptions are to be cast in verification prior to transfer that
    can be made by the client implementation in relation with the
    financial backend.

--- src/exception.ts
+++ src/exception.ts
@@ -97,3 +97,82 @@ export class CanceledOperation extends Error {
         this.name = 'CanceledOperation'
     }
 }
+
+// Payment exceptions
+
+export class PrepareTransferError extends Error {
+    constructor (message) {
+        super(message)
+        this.name = 'PrepareTransferError'
+    }
+}
+
+export class PrepareTransferException extends PrepareTransferError {
+
+    origException: Error
+
+    constructor (message, origException?: Error) {
+        super(message)
+        this.name = 'PrepareTransferException'
+        this.origException = origException
+    }
+}
+
+export class PrepareTransferAmountError extends PrepareTransferError {
+    constructor (message) {
+        super(message)
+        this.name = 'PrepareTransferAmountError'
+    }
+}
+
+export class PrepareTransferInsufficientBalance extends PrepareTransferError {
+    safeAmount: number
+    constructor (message, safeAmount) {
+        super(message)
+        this.name = 'PrepareTransferInsufficientBalance'
+        this.safeAmount = safeAmount
+    }
+}
+
+export class PrepareTransferUnsafeBalance extends PrepareTransferError {
+    realBal: number
+    constructor (message, realBal) {
+        super(message)
+        this.name = 'PrepareTransferUnsafeBalance'
+        this.realBal
+    }
+}
+
+export class PrepareTransferUnsafeSplit extends PrepareTransferError {
+    origException: Error
+    safeAmount: string
+    constructor (message, safeAmount, origException) {
+        super(message)
+        this.name = 'PrepareTransferUnsafeSplit'
+        this.origException = origException
+        this.safeAmount = safeAmount
+    }
+}
+
+// Splitting exceptions
+
+export class RecipientWouldHitCmHighLimit extends Error {
+    safeAmount: number
+    constructor (message, safeAmount) {
+        super(message)
+        this.name = 'RecipientWouldHitCmHighLimit'
+        this.safeAmount = safeAmount
+    }
+}
+
+
+// Invalid Json Data
+
+export class InvalidJsonData extends Error {
+    jsonData: any
+    constructor (message, jsonData) {
+        super(message)
+        this.name = 'InvalidJsonData'
+        this.jsonData = jsonData
+    }
+}
