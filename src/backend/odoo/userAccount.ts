ed79549f6f5364dcf531506e3df03e9f2c2fb442
Valentin Lab <valentin.lab@kalysto.org>
2025-05-15 15:17:01 +0200

    new: add ``Recipient.isTransferAllowedByAdministrativeBackend()``

--- src/backend/odoo/userAccount.ts
+++ src/backend/odoo/userAccount.ts
@@ -2,8 +2,12 @@ import { BridgeObject } from '..'
 import { t } from '../..'
 
 
-export default class UserAccount extends BridgeObject {
+export default abstract class UserAccount extends BridgeObject {
+
+    abstract internalId: string
+
     get isTopUpAllowed() {
         return this.jsonData?.is_topup_allowed !== false
     }
+
 }
